e25c438dadbb2760fec1b46437fb98f10e6ea885
Alex Land <aland@sfu.ca>
2017-01-21 09:53:03 -0800

    Moving stuff around

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RecipeList from './RecipeList.js';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+const App = () => (
+    <MuiThemeProvider>
+        <RecipeList />
+    </MuiThemeProvider>
+);
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <App />,
+    document.getElementById('root')
 );
